bd626628cfc88e76a2f189727f5bb01a18d69283
forestheims <forestheims@gmail.com>
2022-03-15 14:57:52 -0700

    created the colors table with three sample rows on setup.sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS colors;
+
+CREATE TABLE colors (
+    hex TEXT NOT NULL,
+    rgb TEXT NOT NULL,
+    hsl TEXT NOT NULL,
+    name TEXT
+);
+
+INSERT INTO
+  colors (hex, rgb, hsl, name)
+VALUES
+  ('02bfde', '2,191,222', '188,98,44', undefined),
+  ('ff69b4', '255,105,180', '330,100,71', 'HotPink'),
+  ('bbf099', '187,240,153', '97,74,77', undefined);
